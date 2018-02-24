54986afa7bfe6bb8a56503b41da4dcb47b8a25a6
少文 <lishaowen@dowedo.com.cn>
2018-02-24 18:01:32 +0800

    界面陆续完善

--- pages/member_detail/member_detail.js
+++ pages/member_detail/member_detail.js
@@ -5,31 +5,75 @@ Page({
    * 页面的初始数据
    */
   data: {
-    bike_type:["48V","60V","72V","其他"],
+    battery_capacity: ["48V", "60V", "72V", "其他"],
     brand: [
-      "爱玛", "雅迪", "新日", "小牛", 
-      "E客", "台铃", "速珂", "小刀", 
-      "绿源", "立马", "小米", "新大洲", 
+      "爱玛", "雅迪", "新日", "小牛",
+      "E客", "台铃", "速珂", "小刀",
+      "绿源", "立马", "小米", "新大洲",
       "安马达", "大阳", "超威"
-      ],
-    type:[
-
-    ]
-
+    ],
+    brand_index: 0,
+    battery_index: 0,
+    status: 'new',
+    modalFlag: false,
+    imageUrl:"http://47.97.194.247/api/member/captcha"
   },
 
   onLoad: function (options) {
-  
+
   },
 
   brandChange: function (e) {
     this.setData({
-      index: e.detail.value
+      brand_index: e.detail.value
     })
   },
-  bindDateChange: function (e) {
+  buyTimeChange: function (e) {
     this.setData({
-      date: e.detail.value
+      buy_time: e.detail.value
     })
   },
+  batteryCapacityChange: function (e) {
+    this.setData({
+      battery_index: e.detail.value
+    })
+  },
+
+  batteryChangeTime: function (e) {
+    this.setData({
+      battery_change_time: e.detail.value
+    })
+  },
+  tapNew: function () {
+    this.setData({
+      status: 'new'
+    })
+  },
+  tapOld: function () {
+    this.setData({
+      status: 'old'
+    })
+  },
+  getCaptcha: function (e) {
+    this.setData({
+      modalFlag: false
+    });
+  },
+
+  model_cancel:function(e){
+    this.setData({
+      modalFlag: true
+    });
+  },
+
+  model_confirm:function(e){
+    console.log(e);
+  },
+
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
+  formReset: function () {
+    console.log('form发生了reset事件')
+  }
 })
