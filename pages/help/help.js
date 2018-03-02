372a04800e58f5230f795ce6fb16b7baed100597
少文 <lishaowen@dowedo.com.cn>
2018-03-02 18:13:16 +0800

    help界面完善中

--- pages/help/help.js
+++ pages/help/help.js
@@ -1,66 +1,25 @@
-// pages/help/help.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    problem:[
+      "爆胎",
+      "漏气",
+      "电机不转",
+      "断电",
+      "无法启动",
+      "莫名异响",
+      "骑行费力，速度慢",
+      "其他",
+    ],
+    problem_index:0
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  problemChange:function(e){
+    this.setData({
+      problem_index: e.detail.value
+    })
   }
 })
