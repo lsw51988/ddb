3b2706b50042f52dd9e0a092ae29a08ea3cd1e39
lsw <1170394858@qq.com>
2018-03-05 23:18:38 +0800

    页面完善

--- pages/help/help.js
+++ pages/help/help.js
@@ -15,11 +15,12 @@ Page({
     cap_btn_text: "获取验证码",
     cap_loading_status: false,
     cap_btn_status: false,
-    modalFlag: true,
     imageUrl: app.globalData.host + "/api/member/captcha",
     latitude: "",
     longitude: "",
-    mapCtx: ""
+    mapCtx: "",
+    captcha: "",
+    noNeedCaptcha:false
   },
 
   onLoad: function (options) {
@@ -62,67 +63,55 @@ Page({
     })
   },
   getCaptcha: function (e) {
-    this.setData({
-      modalFlag: false,
-      imageUrl: this.data.imageUrl + "?_t=" + new Date().getTime() + "&token=" + wx.getStorageSync("member").token
-    });
+    var that = this;
+    wx.showLoading({
+      title: '请稍后...',
+    })
+    if (!that.data.noNeedCaptcha){
+      wx.request({
+        url: app.globalData.host + '/api/member/verifyCaptcha?captcha=' + that.data.captcha,
+        header: {
+          'content-type': "application/x-www-form-urlencoded",
+          'token': wx.getStorageSync("member").token
+        },
+        method: "GET",
+        success: function (res) {
+          wx.hideLoading();
+          if (res.data.status == true) {
+            //请求短信接口
+            sendCaptcha(that);
+          } else {
+            wx.showModal({
+              title: '提示',
+              showCancel: false,
+              content: '验证码填写错误',
+              success: function () {
+                that.setData({
+                  imageUrl: that.data.imageUrl + "?token=" + wx.getStorageSync("member").token + "&_t=" + new Date().getTime()
+                });
+              }
+            })
+          }
+        },
+        fail: function (res) {
+          wx.hideLoading();
+        }
+      })
+    }else{
+      sendCaptcha(that);
+    }
   },
-  captchaBlur: function (e) {
-    this.data.captcha = e.detail.value;
-  },
+
   freshCaptcha: function (e) {
     this.setData({
       imageUrl: this.data.imageUrl + "?token=" + wx.getStorageSync("member").token + "&_t=" + new Date().getTime()
     });
   },
-  model_cancel: function (e) {
-    this.setData({
-      modalFlag: true
-    });
+  captchaBlur:function(e){
+    console.log(e.detail.value);
+    this.data.captcha = e.detail.value;
   },
-  model_confirm: function (e) {
-    var that = this;
-    wx.showLoading({
-      title: '请稍后...',
-    })
-    wx.request({
-      url: app.globalData.host + '/api/member/verifyCaptcha?captcha=' + this.data.captcha,
-      header: {
-        'content-type': "application/x-www-form-urlencoded",
-        'token': wx.getStorageSync("member").token
-      },
-      method: "GET",
-      success: function (res) {
-        if (res.data.status == true) {
-          //请求短信接口
-          that.setData({
-            cap_btn_status: true,
-            modalFlag: true,
-            cap_loading_status: true
-          });
-          var i = 0;
-          var timer = setInterval(function () {
-            that.setData({
-              cap_btn_text: (59 - i) + "秒"
-            });
-            i++;
-            if (i == 60) {
-              clearInterval(timer);
-              that.setData({
-                cap_btn_status: false,
-                cap_btn_text: "获取验证码",
-                cap_loading_status: false
-              });
-            }
-          }, 1000);
-        }
-        wx.hideLoading();
-      },
-      fail: function (res) {
-        wx.hideLoading();
-      }
-    })
-  },
+
   regionChange: function (e) {
     var that = this;
     that.data.mapCtx.getCenterLocation({
@@ -132,9 +121,55 @@ Page({
       }
     });
   },
-  controltap:function(e){
+  controltap: function (e) {
     //重新定位
-    console.log("重新定位");
     this.data.mapCtx.moveToLocation();
+    this.data.mapCtx.getCenterLocation({
+      success: function (e) {
+        that.data.latitude = e.latitude;
+        that.data.longitude = e.longitude;
+      }
+    })
+  },
+
+  formSubmit: function (e) {
+    var formData = e.detail.value;
+    formData.type = e.detail.target.dataset.id;
+    for (var key in formData) {
+      if (formData[key] === "" || formData[key] === null) {
+        wx.showModal({
+          title: '提示',
+          content: '请填写必填项',
+        })
+        return false;
+      }
+    }
   }
 })
+
+function sendCaptcha(that){
+  that.setData({
+    cap_btn_status: true,
+    cap_loading_status: true,
+  });
+  wx.showModal({
+    title: '提示',
+    content: '已经发送验证码',
+    showCancel: false
+  })
+  var i = 0;
+  var timer = setInterval(function () {
+    that.setData({
+      cap_btn_text: (59 - i) + "秒"
+    });
+    i++;
+    if (i == 60) {
+      clearInterval(timer);
+      that.setData({
+        cap_btn_status: false,
+        cap_btn_text: "获取验证码",
+        cap_loading_status: false
+      });
+    }
+  }, 1000);
+}
