83d29593088487e351a07f8b45f631000e9034df
lsw <1170394858@qq.com>
2018-04-05 13:49:18 +0800

    页面基本完成

---
+++ pages/shb-list/shb-list.js
@@ -0,0 +1,15 @@
+Page({
+  data: {
+    time: "时间",
+    addr: "地区",
+    status: "状态",
+  },
+
+  onLoad: function (options) {
+
+  },
+
+  timeChange: function () { },
+  addrChange: function () { },
+  statusChange: function () { },
+})
