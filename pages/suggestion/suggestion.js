a09f197fe7e216b2fb3fca9d68e05c3ecdaa12a6
少文 <lishaowen@dowedo.com.cn>
2018-05-29 15:53:21 +0800

    建议完成

--- pages/suggestion/suggestion.js
+++ pages/suggestion/suggestion.js
@@ -1,33 +1,68 @@
 // 建议页面
+var util = require("../../utils/util.js");
+const app = getApp()
 Page({
+    data: {
+        checkboxItems: [
+            { name: '页面美观', value: '0' },
+            { name: '操作复杂.', value: '1' }
+        ],
+        types:[]
+    },
 
-  data: {
-    checkboxItems: [
-      { name: '页面美观', value: '0' },
-      { name: '操作复杂.', value: '1' }
-    ],
-  },
+    onLoad: function (options) {
 
-  onLoad: function (options) {
+    },
 
-  },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
-
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
+    checkboxChange: function (e) {
+        var that = this;
+        var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+        for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+            checkboxItems[i].checked = false;
+            for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+                if (checkboxItems[i].value == values[j]) {
+                    checkboxItems[i].checked = true;
+                    break;
+                }
+            }
         }
-      }
-    }
 
-    this.setData({
-      checkboxItems: checkboxItems
-    });
-  },
+        this.setData({
+            checkboxItems: checkboxItems
+        });
+        that.data.types = e.detail.value;
+    },
+
+    formSubmit:function(e){
+        var data = {};
+        data.type = this.data.types;
+        data.content = e.detail.value.content;
+        wx.showLoading({
+            title: '请稍后...',
+        })
+        wx.request({
+            url: app.globalData.host + '/wechat/suggestion/create',
+            method: "POST",
+            header: util.header(),
+            data: data,
+            success: function (res) {
+                wx.hideLoading();
+                if (res.data.status == true) {
+                    wx.hideLoading();
+                    wx.showModal({
+                        title: '提示',
+                        content: '操作成功',
+                        success: function () {
+                            wx.navigateBack()
+                        }
+                    })
+                } else {
+                    util.falseHint(res.data.msg);
+                }
+            },
+            fail: function (res) {
+                util.failHint();
+            }
+        })
+    }
 })
