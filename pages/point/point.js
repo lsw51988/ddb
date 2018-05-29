deac4988f30c044eeda910f27bd1e5cc9e3a06d9
少文 <lishaowen@dowedo.com.cn>
2018-05-29 22:30:44 +0800

    提交内容

--- pages/point/point.js
+++ pages/point/point.js
@@ -4,6 +4,7 @@ Page({
     amount:10
   },
 
+//md5.hexMD5
   onLoad: function (options) {
       console.log(md5.hexMD5("appid=wxd930ea5d5a258f4f&body=test&device_info=1000&mch_id=10000100&nonce_str=ibuaiVcKdpRxkhJA&key=192006250b4c09247ec02edce69f6a2d"));
   },
